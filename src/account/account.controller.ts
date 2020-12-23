0519d035a920988a5b8f9d8ff56aa6557ddfa952
Jonathan Schmold <me@jonathanschmold.ca>
2020-12-23 15:20:59 -0700

    Added some documentation

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -4,6 +4,10 @@ import { RegisterDTO } from './dto/register.dto';
 import { AccountService } from '@app/account/account.service';
 import { ConfirmAccountDTO } from './dto/confirm.dto';
 
+
+/**
+ * The controller in charge of all things managing the user's account, including registration
+ */
 @Controller('account')
 export class AccountController {
 
@@ -11,6 +15,11 @@ export class AccountController {
     private readonly accounts: AccountService
   ) {}
 
+  /**
+   * Register a new account with a new email address. This throws an error if there is already 
+   * an account with an email address provided, and will fail validation in the situation that
+   * the user gave a broken email, or a password of insufficient length / too long.
+   */
   @Post()
   public async register(
     @Body() body: RegisterDTO
@@ -19,6 +28,11 @@ export class AccountController {
     return new MessageResponse('Automatic login is next');
   }
 
+  /**
+   * Using a code that a user received in their email inbox, this takes the code and confirms
+   * their account. Confirmation only works once, and will throw a Conflict if the user has already
+   * confirmed their account.
+   */
   @Post('confirm')
   public async confirmAccount(
     @Body() body: ConfirmAccountDTO
